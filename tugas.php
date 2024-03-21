02bfbd0dfea5d46290ab596b749c9e0a7b41c3ee
Destinaaql <139562603+Destinaaql@users.noreply.github.com>
2024-03-21 11:44:41 +0700

    Add files via upload

---
+++ tugas.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test buta warna</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <h2 style="text-align: center;">Tes Buta Warna</h2>
+    <form action="jawaban_benar1.php" method="post">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                <img src="1.jpg" width="250px">
+                <input type="text" name="jwbn1" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+            <div class="col">
+                <img src="2.jpg" width="250px">
+                <input type="text" name="jwbn2" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+            <div class="col">
+                <img src="3.jpg" width="250px">
+                <input type="text" name="jwbn3" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <img src="4.jpg" width="250px">
+                <input type="text" name="jwbn4" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+            <div class="col">
+                <img src="5.jpg" width="250px">
+                <input type="text" name="jwbn5" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+            <div class="col">
+                <img src="6.jpg" width="250px">
+                <input type="text" name="jwbn6" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <img src="7.jpg" width="250px">
+                <input type="text" name="jwbn7"class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+            <div class="col">
+                <img src="8.jpg" width="250px">
+                <input type="text" name="jwbn8" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+            <div class="col">
+                <img src="9.jpg" width="250px">
+                <input type="text" name="jwbn9" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <img src="10.jpg" width="250px">
+                <input type="text" name="jwbn10" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+            <div class="col">
+                <img src="11.jpg" width="250px">
+                <input type="text" name="jwbn11" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+            <div class="col">
+                <img src="12.jpg" width="250px">
+                <input type="text" name="jwbn12" class="border rounded-3 p-1 text-center" placeholder="jawaban">
+            </div>
+        </div> <br><br>
+        <input type="submit" value="hitung" class="btn btn-primary text-center ms-5">
+    </div>
+    </form>
+</body>
+</html>
